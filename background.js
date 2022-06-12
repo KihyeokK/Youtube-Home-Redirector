72e1be84e8f2eecba30238dd80b99942ab23a119
KihyeokK <joe.brighter20@gmail.com>
2022-06-12 14:34:54 -0400

    disable request for youtube home page

---
+++ background.js
@@ -0,0 +1,15 @@
+const youtubeUrl = "https://www.youtube.com";
+const newUrl = "https://www.youtube.com/results?search_query=proramming";
+
+chrome.runtime.onInstalled.addListener(() => {
+    chrome.storage.sync.set({ youtubeUrl });
+    console.log(youtubeUrl)
+  });
+
+// const filter = {urls: ["https://*/*"], types: ['main_frame', 'sub_frame']};
+// chrome.webRequest.onBeforeRequest.addListener(redirect, filter, ["blocking"]);
+
+// function redirect(details) {
+//     console.log("yeah", details);
+//     return { redirectUrl: newUrl };
+// }
